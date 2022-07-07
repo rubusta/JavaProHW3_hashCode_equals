f18899e40cc48f1406a2b3744e6222a86028819a
rubusta <n.a.rubusta3@gmail.com>
2022-07-07 22:01:51 +0300

    Update2

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
         Transport Car = new Transport(456, "Porsche");
         Transport Car2 = new Transport(678, "Mercedes");
 
-
         set.add(Bike);
         set.add(Car);
         set.add(Car2);
@@ -17,18 +16,18 @@ public class Main {
                 + set + "\n-----------------------");
 
         System.out.println("Equals of Porsche and Mercedes: " + Objects.equals(Car2, Car));
-        System.out.println("Equals of Mercedes and Kawasaki: " + Objects.equals(Car2,Bike)
-        + "\n------------------------");
+        System.out.println("Equals of Mercedes and Kawasaki: " + Objects.equals(Car2, Bike)
+                + "\n------------------------");
 
         System.out.println("HashCode of Kawwasaki: " + Bike.hashCode());
         System.out.println("HashCode of Porche is : " + Car.hashCode());
         System.out.println("HashCode of Mercedes is: " + Car2.hashCode()
-        + "\n------------------------");
+                + "\n------------------------");
 
-  //      System.out.println("The number of Parking for  Kawasaki and  Kawasaki is the same?: "
- //               + Bike.equals(Bike));
-  //      System.out.println("The number of Parking for Kawasaki and Porsche is the same?: "
-  //              + Bike.equals(Car) + "\nThe number of Parking for Mercedes and Porsche is the same?: "
-  //              + Car2.equals(Car));
+        System.out.println("The number of Parking for  Kawasaki and  Kawasaki is the same?: "
+                + Bike.equals(Bike));
+        System.out.println("The number of Parking for Kawasaki and Porsche is the same?: "
+                + Bike.equals(Car) + "\nThe number of Parking for Mercedes and Porsche is the same?: "
+                + Car2.equals(Car));
     }
 }
